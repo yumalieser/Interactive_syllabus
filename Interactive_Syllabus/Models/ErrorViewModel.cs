34467c5a7462a7b8c8ec7c7132a726391d21a20e
yumalieser <216051010@bingol.edu.tr>
2024-05-04 18:04:35 +0300

    Project Begin Files
    Project Begin Files

---
+++ Interactive_Syllabus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Interactive_Syllabus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
