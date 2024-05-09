111f4f7c08e24ec50f41585fed623b803b907ec9
yumalieser <216051010@bingol.edu.tr>
2024-05-10 01:08:28 +0300

    Error Fix

--- Interactive_Syllabus/obj/Debug/net7.0/Interactive_Syllabus.AssemblyInfo.cs
+++ Interactive_Syllabus/obj/Debug/net7.0/Interactive_Syllabus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interactive_Syllabus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec265c94dd804a887d018e840b9e2607abb5661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interactive_Syllabus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interactive_Syllabus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
