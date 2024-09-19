c9098f42e5ce99fc72e269928ee445fcd44ace6b
Sadık Abdusselam Albayrak <s.abdusselam@gmail.com>
2024-09-19 11:22:16 +0300

    Adding StudentController and Views

---
+++ Interactive_Syllabus/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Interactive_Syllabus.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Interactive_Syllabus.Controllers
+{
+	public class StudentController : Controller
+	{
+		Context _context = new Context();
+		public IActionResult Panel()
+		{
+			return View();
+		}
+		public IActionResult Profile()
+		{
+			return View();
+		}
+		public IActionResult Syllabus()
+		{
+			return View();
+		}
+		public IActionResult Objections()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Lessons()
+		{
+			var dersListele = _context.Lessons.ToList();
+			return View(dersListele);
+		}
+	}
+}
